029172696b3d82c069ba12801a2ec69dc6788342
Sankalp Kulkarni <sn573012@dal.ca>
2023-02-10 18:13:18 -0400

    updated log4j configurations for appending logs to file [not working yet]

--- backend/paw-pals/src/main/java/com/asdc/pawpals/controller/VetController.java
+++ backend/paw-pals/src/main/java/com/asdc/pawpals/controller/VetController.java
@@ -7,9 +7,11 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/vet")
+@RestController()
+@RequestMapping("/vet")
 public class VetController {
 
     private static final Logger logger = LogManager.getLogger(VetController.class);
@@ -18,9 +20,9 @@ public class VetController {
     VetService vetService;
 
     @GetMapping("/{id}")
-    public String getVetById(@PathVariable(name = "id") String id){
+    public String getVetById(@PathVariable Integer id){
         logger.info("VetController :: getVetById :: Entering with Id {}", id);
-        return "";
+        return "Hello "+id;
     }
 
 
