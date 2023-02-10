5cf0867178889137e0cad2a247ed26c054e32202
Sankalp Kulkarni <sn573012@dal.ca>
2023-02-10 15:57:45 -0400

    added log4j2 for logging and its config

--- backend/paw-pals/src/main/java/com/asdc/pawpals/controller/VetController.java
+++ backend/paw-pals/src/main/java/com/asdc/pawpals/controller/VetController.java
@@ -1,14 +1,27 @@
 package com.asdc.pawpals.controller;
 
 import com.asdc.pawpals.service.VetService;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/vet")
 public class VetController {
+
+    private static final Logger logger = LogManager.getLogger(VetController.class);
 
     @Autowired
     VetService vetService;
+
+    @GetMapping("/{id}")
+    public String getVetById(@PathVariable(name = "id") String id){
+        logger.info("VetController :: getVetById :: Entering with Id {}", id);
+        return "";
+    }
 
 
 }
