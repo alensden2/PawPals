4320138ce17f7e74e05707ca6d8cac63d40ae902
Sankalp Kulkarni <sn573012@dal.ca>
2023-02-12 19:15:34 -0400

    added basic flow for accessing medical record by animal id

--- backend/paw-pals/src/main/java/com/asdc/pawpals/controller/MedicalRecordController.java
+++ backend/paw-pals/src/main/java/com/asdc/pawpals/controller/MedicalRecordController.java
@@ -1,14 +1,35 @@
 package com.asdc.pawpals.controller;
 
 import com.asdc.pawpals.service.MedicalRecordService;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/medical-history-management")
 public class MedicalRecordController {
+    Logger logger = LogManager.getLogger(MedicalRecordController.class);
 
-    // @Autowired
-    // MedicalRecordService medicalRecordService;
+    @Autowired
+    MedicalRecordService medicalRecordService;
 
+    // @GetMapping("/records/{record-id}")
+    // public ResponseEntity<Object> get
 
+    @GetMapping("/animals/{animal_id}")
+    public ResponseEntity<Object> getMedicalHistory(
+        @PathVariable(value = "animal_id") Long animalId
+    ){
+        Object medicalRecords = null;
+        if(medicalRecordService != null && animalId != null){
+            medicalRecords = medicalRecordService.retrieveMedicalRecord(animalId);
+        }
+        return ResponseEntity.ok().body(medicalRecords);
+    }
 }
--- backend/paw-pals/src/main/java/com/asdc/pawpals/service/implementation/MedicalRecordServiceImpl.java
+++ backend/paw-pals/src/main/java/com/asdc/pawpals/service/implementation/MedicalRecordServiceImpl.java
@@ -2,6 +2,9 @@ package com.asdc.pawpals.service.implementation;
 
 import com.asdc.pawpals.repository.MedicalRecordRepository;
 import com.asdc.pawpals.service.MedicalRecordService;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
@@ -9,7 +12,16 @@ import org.springframework.stereotype.Component;
 @Lazy
 public class MedicalRecordServiceImpl implements MedicalRecordService {
 
+    Logger logger = LogManager.getLogger(MedicalRecordServiceImpl.class);
+
     @Autowired
     MedicalRecordRepository medicalRecordRepository;
+
+    @Override
+    public Object retrieveMedicalRecord(Long animalId){
+        Object medicalRecords = medicalRecordRepository != null ? medicalRecordRepository.findByAnimalId(animalId) : null;
+        logger.debug("MedicalRecordService :: retrieveMedicalRecord :: medicalRecords are : {}", medicalRecords);
+        return medicalRecords;
+    }
 
 }
