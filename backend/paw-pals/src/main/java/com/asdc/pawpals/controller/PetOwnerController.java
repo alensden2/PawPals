945dfb0a32db1dd52c47cb297cd89e415e4f9f62
Sankalp Kulkarni <sn573012@dal.ca>
2023-02-23 15:23:53 -0400

    Revert "Merge branch 'paw-pals-Feature-Branch-Saumya' into 'dev'"
    This reverts merge request !2

--- backend/paw-pals/src/main/java/com/asdc/pawpals/controller/PetOwnerController.java
+++ backend/paw-pals/src/main/java/com/asdc/pawpals/controller/PetOwnerController.java
@@ -2,7 +2,6 @@ package com.asdc.pawpals.controller;
 
 import com.asdc.pawpals.service.PetOwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,9 +9,5 @@ public class PetOwnerController {
 
     @Autowired
     PetOwnerService petOwnerService;
-    //@GetMapping("/petowners")
-    //public List<PetOwnerDto> getAllPetOwners() {
-  //      List<PetOwner> pawpals = PetOwnerService.getAllPetOwners();
-   //     return UserMapper.toUserDtos(pawpals);
-    //}
+
 }
