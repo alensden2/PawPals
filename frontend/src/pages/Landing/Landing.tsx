2f830c424284737e84e556d96c8c9a4edd87b54d
Ishan Makadia <is984216@dal.ca>
2023-02-25 00:51:45 -0400

    feat: add landing page

--- frontend/src/pages/Landing/Landing.tsx
+++ frontend/src/pages/Landing/Landing.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import { useNavigate } from 'react-router-dom';
+import useStyles from './Landing.styles';
 
-function Landing() {
-  return <div>{'landing page'}</div>;
-}
+const HOME_PAGE_IMAGE = '../../assets/images/animals4.jpg';
+
+const Landing: React.FC = () => {
+  const classes = useStyles();
+  const navigate = useNavigate();
+
+  const onLoginClick = () => {
+    navigate('/signin');
+  };
+
+  const onSignUpClick = () => {
+    navigate('/signup');
+  };
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.leftContainer}>
+        <img src={HOME_PAGE_IMAGE} alt="Product" className={classes.image} />
+      </div>
+      <div className={classes.rightContainer}>
+        <div>
+          <h1 className={classes.pawpalsText}>Paw Pals</h1>
+        </div>
+        <div className={classes.buttonContainer} onClick={onLoginClick}>
+          <div style={{ fontFamily: 'Oswald' }} className={classes.loginText}>
+            Sign In
+          </div>
+          <ChevronRightIcon />
+        </div>
+        <div className={classes.buttonContainer} onClick={onSignUpClick}>
+          <div style={{}} className={classes.signUpText}>
+            Sign Up
+          </div>
+          <ChevronRightIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
