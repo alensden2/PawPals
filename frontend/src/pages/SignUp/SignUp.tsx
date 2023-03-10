c18cfd184dcd2d42fa710b7b1421d29759c8b50e
Ishan Makadia <is984216@dal.ca>
2023-03-10 19:20:15 -0400

    chore: add username field and make api call

--- frontend/src/pages/SignUp/SignUp.tsx
+++ frontend/src/pages/SignUp/SignUp.tsx
@@ -1,19 +1,53 @@
 import React, { useState } from 'react';
-import { Container, Link, IconButton, Typography } from '@material-ui/core';
+import {
+  Container,
+  Link,
+  IconButton,
+  Typography,
+  Select,
+  InputLabel,
+  MenuItem
+} from '@material-ui/core';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import useStyles from './SignUp.styles';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 import { TextField, Button } from '@src/components';
 
+import { RegisterUserApiResponse } from '@src/interfaces/api';
+import { registerUser } from '@src/api/auth';
+
 const SignUp: React.FC = () => {
   const classes = useStyles();
+
+  const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [selectedOption, setSelectedOption] = useState('');
+
   const navigate = useNavigate();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    const response: RegisterUserApiResponse = await registerUser({
+      userName,
+      password,
+      role: selectedOption,
+      email
+    });
+
+    if (response?.data?.error) {
+      // TODO: Toast message of failure
+      console.log(response.data.error);
+      console.log(response.data.message);
+    } else {
+      // TODO: Toast message of success
+    }
+  };
+
+  const handleSelectChange = (event: any) => {
+    setSelectedOption(event.target.value);
   };
 
   const onBackClick = () => {
@@ -34,6 +68,13 @@ const SignUp: React.FC = () => {
         </Typography>
         <form onSubmit={handleSubmit}>
           <TextField
+            label="User name"
+            type="text"
+            value={userName}
+            fullWidth={true}
+            onChange={(event) => setUserName(event.target.value)}
+          />
+          <TextField
             label="Email"
             type="email"
             value={email}
@@ -47,6 +88,18 @@ const SignUp: React.FC = () => {
             fullWidth={true}
             onChange={(event) => setPassword(event.target.value)}
           />
+          <div className={classes.selectDropdownContainer}>
+            <InputLabel>Select Role</InputLabel>
+            <Select
+              value={selectedOption}
+              onChange={handleSelectChange}
+              className={classes.selectDropdown}
+            >
+              <MenuItem value="VET">Vet</MenuItem>
+              <MenuItem value="PET_OWNER">Pet Owner</MenuItem>
+              <MenuItem value="ROLE_ADMIN">Admin</MenuItem>
+            </Select>
+          </div>
           <Button
             type="submit"
             variant="contained"
