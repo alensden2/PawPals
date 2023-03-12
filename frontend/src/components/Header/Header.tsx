ebf9c95842efbe89fc3064de507d1f835991faec
Ishan Makadia <is984216@dal.ca>
2023-03-12 14:58:20 -0300

    feat: add Header component

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { ExitToApp } from '@material-ui/icons';
+import useStyles from './Header.styles';
+import PAW_LOGO from '@src/assets/images/paw-logo.png';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+
+type HeaderProps = {
+  title: string;
+  shouldShowBackButton: boolean;
+  shouldShowLogoutButton: boolean;
+};
+
+const Header: React.FC<HeaderProps> = ({
+  title,
+  shouldShowBackButton,
+  shouldShowLogoutButton
+}) => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static" className={classes.appBar}>
+      <Toolbar>
+        {shouldShowBackButton ? (
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="back"
+            className={classes.clickable}
+          >
+            <ArrowBackIosIcon className="back-button-icon" />
+          </IconButton>
+        ) : null}
+        <div>
+          <img
+            src={PAW_LOGO}
+            alt="Paw Pals Logo"
+            className={classes.logo}
+            width={28}
+            height={28}
+          />
+        </div>
+        <Typography variant="h5" className={classes.title}>
+          {title}
+        </Typography>
+        <div className={classes.grow}></div>
+        {shouldShowLogoutButton ? (
+          <>
+            <Typography
+              variant="body1"
+              className={classes.clickable}
+              //   onClick={onLogoutClick}
+            >
+              Logout
+            </Typography>
+            <IconButton
+              color="inherit"
+              className={classes.clickable}
+              //   onClick={onLogoutClick}
+            >
+              <ExitToApp />
+            </IconButton>
+          </>
+        ) : null}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,5 +1,6 @@
 import TextField from './TextField/TextField';
 import Button from './Button/Button';
 import Toast from './Toast/Toast';
+import Header from './Header/Header';
 
-export { TextField, Button, Toast };
+export { TextField, Button, Toast, Header };
