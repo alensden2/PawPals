d75f049e33ddbe9db70c5321622abf688d09593f
Ishan Makadia <is984216@dal.ca>
2023-03-12 15:02:54 -0300

    feat: render Header in Root

--- frontend/src/pages/Root/Root.tsx
+++ frontend/src/pages/Root/Root.tsx
@@ -1,13 +1,26 @@
 import React, { useContext } from 'react';
-import { Toast } from '@src/components';
-import { ToastContext } from '@src/context';
+import { Toast, Header } from '@src/components';
+import { ToastContext, HeaderContext } from '@src/context';
 import { Outlet } from 'react-router-dom';
 
 function Root() {
   const { toast } = useContext(ToastContext);
+  const {
+    shouldShowHeader,
+    title,
+    shouldShowBackButton,
+    shouldShowLogoutButton
+  } = useContext(HeaderContext);
 
   return (
     <div>
+      {shouldShowHeader ? (
+        <Header
+          title={title}
+          shouldShowBackButton={shouldShowBackButton}
+          shouldShowLogoutButton={shouldShowLogoutButton}
+        />
+      ) : null}
       <Toast toast={toast} />
       <Outlet />
     </div>
