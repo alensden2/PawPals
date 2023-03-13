08c652ac47a6ac0021baf066834bdf281f5e03a5
Ishan Makadia <is984216@dal.ca>
2023-03-13 01:25:47 -0300

    chore: add empty state

---
+++ frontend/src/components/EmptyState/EmptyState.tsx
@@ -0,0 +1,34 @@
+import { Box, Typography } from '@material-ui/core';
+import NOTHING_HERE_YET from '@src/assets/images/nothing_here_yet.png';
+import useStyles from './EmptyState.styles';
+
+function EmptyState() {
+  const classes = useStyles();
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      mt={8}
+      className={classes.boxContainer}
+    >
+      <img
+        src={NOTHING_HERE_YET}
+        alt="Empty state icon"
+        width={560}
+        height={500}
+      />
+      <Typography
+        variant="h5"
+        align="center"
+        color="textSecondary"
+        gutterBottom
+      >
+        Nothing here yet!
+      </Typography>
+    </Box>
+  );
+}
+
+export default EmptyState;
