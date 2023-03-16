61136706cf2150cf12b16be63987eb6575e6b317
Ishan Makadia <is984216@dal.ca>
2023-03-16 14:15:55 -0300

    feat: add Loader component

---
+++ frontend/src/components/Loader/Loader.styles.tsx
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%'
+  },
+  loadingText: {
+    marginTop: theme.spacing(1)
+  }
+}));
+
+export default useStyles;
