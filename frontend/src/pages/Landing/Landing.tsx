0c82af3ba593a141a1af16f7a173ec812a47a34a
Saumya Shah <sm351623@dal.ca>
2023-03-20 20:09:38 -0300

    add app bar

--- frontend/src/pages/Landing/Landing.tsx
+++ frontend/src/pages/Landing/Landing.tsx
@@ -19,9 +19,91 @@ const Landing: React.FC = () => {
   const classes = useStyles();
   const navigate = useNavigate();
 
+  const onLoginClick = () => {
+    navigate('signin');
+  };
+
+  const onSignUpClick = () => {
+    navigate('signup');
+  };
+
+  const onHomeClick = () => {
+    navigate('/');
+  };
+
+  const onAboutUsClick = () => {
+    navigate('/about-us');
+  };
+
+  const onContactUsClick = () => {
+    navigate('/contact-us');
+  };
 
   return (
     <>
+      <AppBar className={classes.header}>
+        <Toolbar>
+          <Link href="/" color="inherit" className={classes.logo}>
+            <div className={classes.headerTitleContainer}>
+              <img
+                src={PAW_LOGO}
+                alt="Paw Pals Logo"
+                className={classes.logo}
+                width={28}
+                height={28}
+              />
+              <Typography variant="h5">{'PawPals'}</Typography>
+            </div>
+          </Link>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="end"
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <nav className={classes.navLinks}>
+            <Link
+              color="inherit"
+              className={classes.navLink}
+              onClick={onHomeClick}
+            >
+              {'Home'}
+            </Link>
+            <Link
+              color="inherit"
+              className={classes.navLink}
+              onClick={onAboutUsClick}
+            >
+              {'About Us'}
+            </Link>
+            <Link
+              color="inherit"
+              className={classes.navLink}
+              onClick={onContactUsClick}
+            >
+              {'Contact Us'}
+            </Link>
+            <Button
+              variant="outlined"
+              color="inherit"
+              onClick={onLoginClick}
+              className={classes.navLink}
+            >
+              {'Login'}
+            </Button>
+            <Button
+              variant="outlined"
+              color="inherit"
+              onClick={onSignUpClick}
+              className={classes.navLink}
+            >
+              {'Sign Up'}
+            </Button>
+          </nav>
+        </Toolbar>
+      </AppBar>
     </>
   );
 };
