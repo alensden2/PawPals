0f4da5929f4cf7b346b9b8fbd5f1955d615e09e0
Ishan Makadia <is984216@dal.ca>
2023-03-23 17:20:53 -0300

    feat: add MedicalRecordCardList

---
+++ frontend/src/pages/PetMedicalRecord/MedicalRecordCardList/MedicalRecordCardList.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { MedicalRecordList } from '@src/types';
+import MedicalRecordCard from '../MedicalRecordCard';
+import useStyles from './MedicalRecordCardList.styles';
+
+const MedicalRecordCardList: React.FC<MedicalRecordList> = ({
+  medicalRecords
+}) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      {medicalRecords.map((item) => (
+        <MedicalRecordCard key={item.medicalHistoryId} medicalHistory={item} />
+      ))}
+    </div>
+  );
+};
+
+export default MedicalRecordCardList;
