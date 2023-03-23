1e8d363209a2834aa0e9c5f90e7688080c11d840
Ishan Makadia <is984216@dal.ca>
2023-03-23 17:11:27 -0300

    feat: add medHistoryData

---
+++ frontend/src/data/medhistory.js
@@ -0,0 +1,73 @@
+export const medHistoryData = {
+  medicalRecords: [
+    {
+      medicalHistoryId: 1,
+      dateDiagnosed: '2022-01-15',
+      prescription: 'Ibuprofen',
+      vaccines: 'Flu, COVID-19',
+      pet: {
+        id: 1,
+        age: 2,
+        gender: 'Female',
+        name: 'Luna',
+        type: 'Cat'
+      },
+      vet: {
+        vetId: 1,
+        name: 'John Doe',
+        phoneNo: '123-456-7890',
+        email: 'johndoe@gmail.com',
+        experience: 5,
+        licenseNumber: '12345',
+        qualification: 'MD',
+        clinicAddress: '123 Main St, Anytown USA'
+      }
+    },
+    {
+      medicalHistoryId: 2,
+      dateDiagnosed: '2022-01-15',
+      prescription: 'Ibuprofen',
+      vaccines: 'Flu, COVID-19',
+      pet: {
+        id: 1,
+        age: 2,
+        gender: 'Female',
+        name: 'Luna',
+        type: 'Cat'
+      },
+      vet: {
+        vetId: 1,
+        name: 'John Doe',
+        phoneNo: '123-456-7890',
+        email: 'johndoe@gmail.com',
+        experience: 5,
+        licenseNumber: '12345',
+        qualification: 'MD',
+        clinicAddress: '123 Main St, Anytown USA'
+      }
+    },
+    {
+      medicalHistoryId: 3,
+      dateDiagnosed: '2022-01-15',
+      prescription: 'Ibuprofen',
+      vaccines: 'Flu, COVID-19',
+      pet: {
+        id: 1,
+        age: 2,
+        gender: 'Female',
+        name: 'Luna',
+        type: 'Cat'
+      },
+      vet: {
+        vetId: 1,
+        name: 'John Doe',
+        phoneNo: '123-456-7890',
+        email: 'johndoe@gmail.com',
+        experience: 5,
+        licenseNumber: '12345',
+        qualification: 'MD',
+        clinicAddress: '123 Main St, Anytown USA'
+      }
+    }
+  ]
+};
