93a7b7cf0c2de3bbbf1884d5277aac22c2b29929
Ishan Makadia <is984216@dal.ca>
2023-03-26 17:52:35 -0300

    fix: import styles

--- frontend/src/pages/VetHome/AppointmentDetailsModal/AppointmentDetailsModal.jsx
+++ frontend/src/pages/VetHome/AppointmentDetailsModal/AppointmentDetailsModal.jsx
@@ -14,6 +14,7 @@ import {
   Box
 } from '@material-ui/core';
 import { CloseOutlined } from '@material-ui/icons';
+import useStyles from './AppointmentDetailsModal.styles';
 
 const medicalRecord = 'This is the medical record tab';
 
