1180744e6fcf21739429e26001e48d5062df5c94
Ishan Makadia <is984216@dal.ca>
2023-03-27 00:34:46 -0300

    refactor: VetCard

---
+++ frontend/src/pages/PetOwnerAllVets/VetCard/VetCard.styles.jsx
@@ -0,0 +1,25 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 600,
+    width: '100%'
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%' // 16:9
+  },
+  headerContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    background: '#ECF0F1',
+    padding: '16px'
+  },
+  actionButtons: {
+    display: 'flex',
+    gap: '16px'
+  }
+});
+
+export default useStyles;
