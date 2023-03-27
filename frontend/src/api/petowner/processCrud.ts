5d2a95011ef80280ecff8a72573f15cddaa2216a
Ishan Makadia <is984216@dal.ca>
2023-03-26 22:01:58 -0300

    chore: remove unused files

--- frontend/src/api/petowner/processCrud.ts
+++
@@ -1,7 +0,0 @@
-import { PetOwner } from '@src/types';
-import { registerPetOwnerApiCall } from './crud';
-
-export const registerPetOwner = async (petOwner: PetOwner) => {
-  const response = await registerPetOwnerApiCall(petOwner);
-  return response;
-};
--- frontend/src/api/vet/processCrud.ts
+++
@@ -1,7 +0,0 @@
-import { Vet } from '@src/types';
-import { registerVetApiCall } from './crud';
-
-export const registerVet = async (vet: Omit<Vet, 'vetUserId'>) => {
-  const response = await registerVetApiCall(vet);
-  return response;
-};
