6ed032e52d9ffd0419aad3caf359f969e65e89d9
Saumya Shah <sm351623@dal.ca>
2023-03-28 14:55:02 -0300

    fix: route to home on vet component

--- frontend/src/pages/Vet/Vet.tsx
+++ frontend/src/pages/Vet/Vet.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useNavigate } from '@src/hooks';
 
 const Vet: React.FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('home');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       <Outlet />
