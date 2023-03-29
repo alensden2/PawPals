3b92b3ef125c86bbaf202830ecc2e4c3805bdcde
alensden <alensden@gmail.com>
2023-03-29 12:15:36 -0300

    added update tests

--- backend/paw-pals/src/test/java/com/asdc/pawpals/controller/PetOwnerControllerTest.java
+++ backend/paw-pals/src/test/java/com/asdc/pawpals/controller/PetOwnerControllerTest.java
@@ -1,16 +1,24 @@
 package com.asdc.pawpals.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
+import com.asdc.pawpals.exception.InvalidImage;
+import com.asdc.pawpals.exception.InvalidPetOwnerObject;
+import com.asdc.pawpals.service.PetOwnerService;
+import com.asdc.pawpals.utils.ApiResponse;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.asdc.pawpals.dto.PetOwnerDto;
 import com.asdc.pawpals.exception.NoPetRegisterUnderPetOwner;
@@ -18,6 +26,8 @@ import com.asdc.pawpals.exception.UserNameNotFound;
 import com.asdc.pawpals.service.implementation.PetOwnerImpl;
 
 import net.bytebuddy.agent.VirtualMachine.ForHotSpot.Connection.Response;
+
+import java.io.IOException;
 
 @SpringBootTest
 public class PetOwnerControllerTest {
@@ -48,4 +58,21 @@ public void shouldRegisterPetOwner(){
     //     petToRegister.setUserName("jD");
     // ResponseEntity<String> responese = petOwnerController.registerUser(petToRegister, null);
 }
+
+public void testUpdatedPetOwner() throws InvalidPetOwnerObject, UserNameNotFound, InvalidImage, IOException {
+    PetOwnerDto petOwnerDto = new PetOwnerDto();
+    petOwnerDto.setAddress("new address");
+    MultipartFile image = new MockMultipartFile("image", "test-image.jpg", "image/jpeg", new byte[10]);
+
+    String id = "12";
+    when(petOwnerServiceMock.updatePetOwner(eq(id), eq(petOwnerDto),any(MultipartFile.class))).thenReturn(new PetOwnerDto());
+    Object requestBody = petOwnerDto;
+    ResponseEntity<ApiResponse> response = petOwnerController.updatePetOwner(id,requestBody,image);
+
+    verify(petOwnerServiceMock).updatePetOwner(eq(id), eq(petOwnerDto),any(MultipartFile.class));
+    ApiResponse apiResponse = response.getBody();
+    assertTrue(apiResponse.isSuccess());
+    assertFalse(apiResponse.isError());
 }
+}
+
