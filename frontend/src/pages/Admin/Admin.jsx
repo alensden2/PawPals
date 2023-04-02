9eed33af793a015b812c0cb4d551ac86fb21ea21
Alen John <al283652@dal.ca>
2023-04-02 13:32:18 -0300

    chore: ts to js

--- frontend/src/pages/Admin/Admin.jsx
+++ frontend/src/pages/Admin/Admin.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
-const Admin: React.FC = () => {
+const Admin = () => {
   return (
     <div>
       <div>{'Admin Home Page'}</div>
