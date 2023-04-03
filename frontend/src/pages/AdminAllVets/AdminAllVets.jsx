a56ab71650b2400f3fa7d8c1bbae7894d5d2333b
Saumya Shah <sm351623@dal.ca>
2023-04-03 17:18:13 -0300

    hook table and api in all vets component

--- frontend/src/pages/AdminAllVets/AdminAllVets.jsx
+++ frontend/src/pages/AdminAllVets/AdminAllVets.jsx
@@ -1,13 +1,18 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import useStyles from './AdminAllVets.styles';
 import { HeaderContext } from '@src/context';
+import { getAllVets } from '@src/api';
+import AllVetsTable from './AllVetsTable';
+import { Loader, EmptyState } from '@src/components';
 
 const AdminAllVets = () => {
   const classes = useStyles();
   const { setHeader } = useContext(HeaderContext);
+  const [vets, setVets] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     setHeader({
@@ -17,10 +22,35 @@ const AdminAllVets = () => {
       shouldShowBackButton: true,
       backRoute: '/admin/home'
     });
+
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        const response = await getAllVets();
+        setVets(response);
+        setIsLoading(false);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    fetchData();
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <div>{'Admin All Vets'}</div>;
+  const render = () => {
+    if (isLoading) {
+      return <Loader />;
+    }
+
+    if (vets?.length === 0) {
+      return <EmptyState text={'No Veterinarians in the system yet!'} />;
+    }
+
+    return <AllVetsTable vets={vets} />;
+  };
+
+  return <div className={classes.root}>{render()}</div>;
 };
 
 export default AdminAllVets;
