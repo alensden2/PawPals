ea1d04b41d2a1977908c37b66ad1f2ed8f84252a
Ishan Makadia <is984216@dal.ca>
2023-04-03 18:25:26 -0300

    chore (api): add get all pet owner api call

--- frontend/src/api/admin/processCrud.js
+++ frontend/src/api/admin/processCrud.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
-import { getAllVetsApiCall } from './crud';
+import { getAllVetsApiCall, getAllPetOwnersApiCall } from './crud';
 import { getImageUrlFromBytes } from '@src/utils';
 
 export const getAllVets = async () => {
@@ -13,6 +13,20 @@ export const getAllVets = async () => {
       ...item,
       clinicUrl: item.clinicUrl
         ? getImageUrlFromBytes({ bytes: item.clinicUrl })
+        : ''
+    };
+  });
+};
+
+export const getAllPetOwners = async () => {
+  const response = await getAllPetOwnersApiCall();
+  const data = response?.data || [];
+
+  return data.map((item) => {
+    return {
+      ...item,
+      photoUrl: item?.photoUrl
+        ? getImageUrlFromBytes({ bytes: item.photoUrl })
         : ''
     };
   });
