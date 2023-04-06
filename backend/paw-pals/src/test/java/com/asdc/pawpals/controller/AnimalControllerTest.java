89f3257b860fc87b2ea72dd8d90524ec2f0aaa7d
Alen John <alensden@gmail.com>
2023-04-06 16:44:30 -0300

    Animal test cases -delete Animal

--- backend/paw-pals/src/test/java/com/asdc/pawpals/controller/AnimalControllerTest.java
+++ backend/paw-pals/src/test/java/com/asdc/pawpals/controller/AnimalControllerTest.java
@@ -8,15 +8,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
-import com.asdc.pawpals.dto.AnimalDto;
-import com.asdc.pawpals.exception.InvalidAnimalId;
-import com.asdc.pawpals.exception.InvalidAnimalObject;
-import com.asdc.pawpals.exception.InvalidImage;
-import com.asdc.pawpals.exception.UserNameNotFound;
-import com.asdc.pawpals.service.implementation.AnimalServiceImpl;
-import com.asdc.pawpals.utils.ApiResponse;
-import com.asdc.pawpals.utils.CommonUtils;
-import io.jsonwebtoken.io.IOException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,6 +18,17 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.asdc.pawpals.dto.AnimalDto;
+import com.asdc.pawpals.exception.InvalidAnimalId;
+import com.asdc.pawpals.exception.InvalidAnimalObject;
+import com.asdc.pawpals.exception.InvalidImage;
+import com.asdc.pawpals.exception.UserNameNotFound;
+import com.asdc.pawpals.service.implementation.AnimalServiceImpl;
+import com.asdc.pawpals.utils.ApiResponse;
+import com.asdc.pawpals.utils.CommonUtils;
+
+import io.jsonwebtoken.io.IOException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AnimalControllerTest {
@@ -148,4 +150,29 @@ public class AnimalControllerTest {
     assertTrue(apiResponse.isSuccess());
     assertFalse(apiResponse.isError());
   }
+
+  @Test
+  public void deleteAnimalTest()
+    throws InvalidAnimalId {
+    //Arrange
+    Long _id = 1L;
+    AnimalDto animalDto = new AnimalDto();
+    //Act
+    when(animalServiceMock.deleteAnimal(anyLong())).thenReturn(animalDto);
+    when(apiResponseMock.getBody()).thenReturn(animalDto);
+    when(apiResponseMock.isSuccess()).thenReturn(true);
+    when(apiResponseMock.isError()).thenReturn(false);
+
+    ResponseEntity<ApiResponse> response = animalController.deleteAnimal(_id);
+    ApiResponse apiResponse = response.getBody();
+
+    //Assert
+    assertNotNull(response.getBody().getBody());
+    assertEquals(animalDto, response.getBody().getBody());
+    assertTrue(apiResponse.isSuccess());
+    assertFalse(apiResponse.isError());
+  }
+
+
+
 }
