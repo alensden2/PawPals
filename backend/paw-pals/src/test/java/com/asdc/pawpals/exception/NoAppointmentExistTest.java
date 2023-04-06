7c15a6a9048585d83ae0b7da376cb9a3aa21668f
Ishan Makadia <is984216@dal.ca>
2023-04-06 19:13:19 -0300

    chore: add NoAppointmentExistTest exception class

---
+++ backend/paw-pals/src/test/java/com/asdc/pawpals/exception/NoAppointmentExistTest.java
@@ -0,0 +1,43 @@
+package com.asdc.pawpals.exception;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class NoAppointmentExistTest {
+
+    @Test
+    public void testNoAppointmentExist() {
+        NoAppointmentExist e = new NoAppointmentExist();
+        assertNull(e.getMessage());
+        assertNull(e.getCause());
+    }
+
+    @Test
+    public void testNoAppointmentExistWithMessage() {
+        String message = "No appointment exists";
+        NoAppointmentExist e = new NoAppointmentExist(message);
+        assertEquals(message, e.getMessage());
+        assertNull(e.getCause());
+    }
+
+    @Test
+    public void testNoAppointmentExistWithCause() {
+        String causeMessage = "Some cause";
+        Throwable cause = new RuntimeException(causeMessage);
+        NoAppointmentExist e = new NoAppointmentExist(cause);
+        assertEquals(cause, e.getCause());
+        assertEquals(causeMessage, e.getCause().getMessage());
+    }
+
+    @Test
+    public void testNoAppointmentExistWithMessageAndCause() {
+        String message = "No appointment exists";
+        String causeMessage = "Some cause";
+        Throwable cause = new RuntimeException(causeMessage);
+        NoAppointmentExist e = new NoAppointmentExist(message, cause);
+        assertEquals(message, e.getMessage());
+        assertEquals(cause, e.getCause());
+        assertEquals(causeMessage, e.getCause().getMessage());
+    }
+
+}
