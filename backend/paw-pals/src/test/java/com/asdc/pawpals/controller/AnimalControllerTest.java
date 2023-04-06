4b4c8ad0d239c1eab1b1bd7833aa63504d5a8633
Alen John <alensden@gmail.com>
2023-04-06 16:35:54 -0300

    Animal test cases -update Animal

--- backend/paw-pals/src/test/java/com/asdc/pawpals/controller/AnimalControllerTest.java
+++ backend/paw-pals/src/test/java/com/asdc/pawpals/controller/AnimalControllerTest.java
@@ -5,9 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 import com.asdc.pawpals.dto.AnimalDto;
+import com.asdc.pawpals.exception.InvalidAnimalId;
 import com.asdc.pawpals.exception.InvalidAnimalObject;
 import com.asdc.pawpals.exception.InvalidImage;
 import com.asdc.pawpals.exception.UserNameNotFound;
@@ -87,6 +89,56 @@ public class AnimalControllerTest {
     ResponseEntity<ApiResponse> response = animalController.registerAnimal(
       animalDto,
       image
+    );
+    ApiResponse apiResponse = response.getBody();
+
+    //Assert
+    assertNotNull(response.getBody().getBody());
+    assertEquals(animalDto, response.getBody().getBody());
+    assertTrue(apiResponse.isSuccess());
+    assertFalse(apiResponse.isError());
+  }
+
+  @Test
+  public void updateAnimalTest()
+    throws IOException, UserNameNotFound, InvalidImage, InvalidAnimalObject, java.io.IOException, InvalidAnimalId {
+    //Arrange
+    AnimalDto animalDto = new AnimalDto();
+    Long _id = 1L;
+    animalDto.setId(1L);
+    animalDto.setName("Chris");
+    animalDto.setType("Dog");
+    animalDto.setAge(4);
+    animalDto.setGender("Male");
+    animalDto.setOwnerId("123");
+    MockMultipartFile image = new MockMultipartFile(
+      "image.jpg",
+      "image.jpg",
+      "image/jpeg",
+      new byte[] { 12 }
+    );
+
+    // Act
+    Byte[] result = CommonUtils.getBytes(image);
+    try {
+      animalDto.setPhotoUrl(CommonUtils.getBytes(image));
+    } catch (java.io.IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    animalDto.setMedicalHistory(null);
+
+    //Act
+    when(animalServiceMock.updateAnimal(any(AnimalDto.class), anyLong(), any(MultipartFile.class)))
+      .thenReturn(animalDto);
+    when(apiResponseMock.getBody()).thenReturn(animalDto);
+    when(apiResponseMock.isSuccess()).thenReturn(true);
+    when(apiResponseMock.isError()).thenReturn(false);
+
+    ResponseEntity<ApiResponse> response = animalController.updateAnimal(
+      animalDto,
+      image,
+      _id
     );
     ApiResponse apiResponse = response.getBody();
 
