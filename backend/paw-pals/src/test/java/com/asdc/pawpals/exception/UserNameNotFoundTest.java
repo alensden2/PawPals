ddcc39366b4af77dc15c1453e3bfefb8e146a305
Ishan Makadia <is984216@dal.ca>
2023-04-06 19:33:12 -0300

    test: add UserNameNotFoundTest

---
+++ backend/paw-pals/src/test/java/com/asdc/pawpals/exception/UserNameNotFoundTest.java
@@ -0,0 +1,35 @@
+package com.asdc.pawpals.exception;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserNameNotFoundTest {
+
+    @Test
+    public void testConstructorWithNoMessage() {
+        UserNameNotFound ex = new UserNameNotFound();
+        assertEquals(null, ex.getMessage());
+    }
+
+    @Test
+    public void testConstructorWithMessage() {
+        UserNameNotFound ex = new UserNameNotFound("User name not found");
+        assertEquals("User name not found", ex.getMessage());
+    }
+
+    @Test
+    public void testConstructorWithCause() {
+        Throwable cause = new Throwable("Cause of user name not found");
+        UserNameNotFound ex = new UserNameNotFound(cause);
+        assertEquals("Cause of user name not found", ex.getCause().getMessage());
+    }
+
+    @Test
+    public void testConstructorWithMessageAndCause() {
+        Throwable cause = new Throwable("Cause of user name not found");
+        UserNameNotFound ex = new UserNameNotFound("User name not found", cause);
+        assertEquals("User name not found", ex.getMessage());
+        assertEquals("Cause of user name not found", ex.getCause().getMessage());
+    }
+}
