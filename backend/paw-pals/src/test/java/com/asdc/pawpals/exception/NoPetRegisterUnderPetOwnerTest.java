f30a10299585318aa6a620fd234295b5f3e7ba07
Ishan Makadia <is984216@dal.ca>
2023-04-06 19:15:07 -0300

    chore: add NoPetRegisterUnderPetOwnerTest exception class

---
+++ backend/paw-pals/src/test/java/com/asdc/pawpals/exception/NoPetRegisterUnderPetOwnerTest.java
@@ -0,0 +1,39 @@
+package com.asdc.pawpals.exception;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class NoPetRegisterUnderPetOwnerTest {
+
+    @Test
+    public void testConstructorWithNoArguments() {
+        NoPetRegisterUnderPetOwner ex = new NoPetRegisterUnderPetOwner();
+        assertNull(ex.getMessage());
+        assertNull(ex.getCause());
+    }
+
+    @Test
+    public void testConstructorWithMessage() {
+        String message = "No pet registered under this pet owner";
+        NoPetRegisterUnderPetOwner ex = new NoPetRegisterUnderPetOwner(message);
+        assertEquals(message, ex.getMessage());
+        assertNull(ex.getCause());
+    }
+
+    @Test
+    public void testConstructorWithCause() {
+        Throwable cause = new RuntimeException("Some runtime exception");
+        NoPetRegisterUnderPetOwner ex = new NoPetRegisterUnderPetOwner(cause);
+        assertEquals(cause, ex.getCause());
+        assertNotNull(ex.getMessage());
+    }
+
+    @Test
+    public void testConstructorWithMessageAndCause() {
+        String message = "No pet registered under this pet owner";
+        Throwable cause = new RuntimeException("Some runtime exception");
+        NoPetRegisterUnderPetOwner ex = new NoPetRegisterUnderPetOwner(message, cause);
+        assertEquals(message, ex.getMessage());
+        assertEquals(cause, ex.getCause());
+    }
+}
