411f6b1a9afbe21cb0ed67aee7b7a1e24546fa86
abhinavs <abhinav.singh@dal.ca>
2023-04-06 18:30:55 -0300

    validator test cases

--- backend/paw-pals/src/main/java/com/asdc/pawpals/utils/Constants.java
+++ backend/paw-pals/src/main/java/com/asdc/pawpals/utils/Constants.java
@@ -9,6 +9,6 @@ public class Constants {
     public static final String TIME_FORMAT = "HH:mm"; //24 hour format e.g. 23:59:59
     public static final Integer SLOT_DURATION = 30; //in Minutes
 
-    public static final String[] STATUS = {"CONFIRMED", "REJECTED", "PENDING"};
+    public static final String[] STATUS = {"CONFIRMED", "REJECTED", "PENDING","COMPLETED"};
     public static final String[] DAY_OF_WEEK = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 }
