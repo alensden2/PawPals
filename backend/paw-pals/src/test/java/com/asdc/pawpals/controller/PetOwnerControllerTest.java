766dc52054b72c08c36db76297fff51b35a12b7c
Alen John <alensden@gmail.com>
2023-04-07 13:45:01 -0300

    petowner controller test cases get pets by owner id

--- backend/paw-pals/src/test/java/com/asdc/pawpals/controller/PetOwnerControllerTest.java
+++ backend/paw-pals/src/test/java/com/asdc/pawpals/controller/PetOwnerControllerTest.java
@@ -5,29 +5,25 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.asdc.pawpals.dto.PetOwnerDto;
-import com.asdc.pawpals.repository.PetOwnerRepository;
-import com.asdc.pawpals.service.implementation.PetOwnerImpl;
-import com.asdc.pawpals.utils.ApiResponse;
-import com.asdc.pawpals.utils.CommonUtils;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
+
+import com.asdc.pawpals.dto.AnimalDto;
+import com.asdc.pawpals.dto.PetOwnerDto;
+import com.asdc.pawpals.service.implementation.PetOwnerImpl;
+import com.asdc.pawpals.utils.ApiResponse;
+import com.asdc.pawpals.utils.CommonUtils;
 
 @SpringBootTest
 public class PetOwnerControllerTest {
@@ -92,5 +88,85 @@ public class PetOwnerControllerTest {
         assertFalse(apiResponse.isError());
 
   }
+
+  @Test
+public void testGetPetsByOwnerId() throws Exception {
+    // Arrange
+    String ownerId = "1";
+    PetOwnerDto pet1 = new PetOwnerDto();
+    AnimalDto animal1 = new AnimalDto();
+    animal1.setId(1L);
+    animal1.setName("Chris");
+    animal1.setType("Dog");
+    animal1.setAge(4);
+    animal1.setGender("Male");
+    animal1.setOwnerId("123");
+    MockMultipartFile image = new MockMultipartFile(
+      "image.jpg",
+      "image.jpg",
+      "image/jpeg",
+      new byte[] { 12 }
+    );
+
+    // Act
+    Byte[] result = CommonUtils.getBytes(image);
+    try {
+      animal1.setPhotoUrl(CommonUtils.getBytes(image));
+    } catch (java.io.IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    animal1.setMedicalHistory(null);
+    pet1.setPhoneNo("42112322212");
+    pet1.setAddress("123 street");
+
+
+    PetOwnerDto pet2 = new PetOwnerDto();
+    AnimalDto animal2 = new AnimalDto();
+    pet2.setPhoneNo("400000000012");
+    pet2.setAddress("123 ssttrreett");
+    animal2.setId(1L);
+    animal2.setName("Chris");
+    animal2.setType("Dog");
+    animal2.setAge(4);
+    animal2.setGender("Male");
+    animal2.setOwnerId("123");
+    MockMultipartFile image2 = new MockMultipartFile(
+      "image.jpg",
+      "image.jpg",
+      "image/jpeg",
+      new byte[] { 12 }
+    );
+
+    // Act
+    Byte[] result2 = CommonUtils.getBytes(image2);
+    try {
+      animal2.setPhotoUrl(CommonUtils.getBytes(image2));
+    } catch (java.io.IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    animal2.setMedicalHistory(null);
+
+
+    List<AnimalDto> pets = new ArrayList<>();
+    pets.add(animal1);
+    pets.add(animal2);
+
+    when(petOwnerServiceMock.retrieveAllPets(ownerId)).thenReturn(pets);
+    when(apiResponseMock.getBody()).thenReturn(pets);
+    when(apiResponseMock.isSuccess()).thenReturn(true);
+    when(apiResponseMock.isError()).thenReturn(false);
+
+    // Act
+    ResponseEntity<ApiResponse> response = petOwnerController.getPetsByOwnerId(ownerId);
+    ApiResponse apiResponse = response.getBody();
+
+    // Assert
+    assertNotNull(response.getBody().getBody());
+    assertEquals(pets, response.getBody().getBody());
+    assertTrue(apiResponse.isSuccess());
+    assertFalse(apiResponse.isError());
+}
 
 }
