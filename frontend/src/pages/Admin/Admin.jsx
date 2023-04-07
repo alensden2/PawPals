b6f1b6c826fa24d4c71754b8c41598379d242897
Abhinav Singh <abhinav.singh@dal.ca>
2023-04-07 15:45:10 -0300

    chore: add route guard admin

--- frontend/src/pages/Admin/Admin.jsx
+++ frontend/src/pages/Admin/Admin.jsx
@@ -2,13 +2,22 @@ import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useNavigate } from '@src/hooks';
 import useStyles from './Admin.styles';
+import { localStorageUtil } from '@src/utils';
 
 const Admin = () => {
   const navigate = useNavigate();
   const classes = useStyles();
 
   useEffect(() => {
-    navigate('home');
+    const user = localStorageUtil.getItem('user');
+
+    const roles = ['ADMIN', 'ROLE_ADMIN'];
+    if (roles.includes(user?.role)) {
+      navigate('home');
+    } else {
+      navigate('/', { replace: true });
+    }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
