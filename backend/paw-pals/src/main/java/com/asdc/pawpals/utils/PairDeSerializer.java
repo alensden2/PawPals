1699b53ac79746a0e7649184473aaba0a8caf9e2
abhinavs <abhinav.singh@dal.ca>
2023-04-07 21:08:02 -0300

    java docs for controller and utils

--- backend/paw-pals/src/main/java/com/asdc/pawpals/utils/PairDeSerializer.java
+++ backend/paw-pals/src/main/java/com/asdc/pawpals/utils/PairDeSerializer.java
@@ -10,7 +10,21 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 
+/**
+
+ This class deserializes a JSON string into a Pair object of type String and String.
+
+ It extends the JsonDeserializer class from the Jackson library.
+ */
 public class PairDeSerializer extends JsonDeserializer<Pair<String, String>>{
+    /**
+
+     This method deserializes a JSON string into a Pair object of type String and String.
+     @param jp - the JsonParser object to read the JSON string.
+     @param ctxt - the DeserializationContext object.
+     @return a Pair object of type String and String.
+     @throws IOException if an error occurs during the deserialization process.
+     */
     @Override
     public Pair<String,String> deserialize(JsonParser jp, DeserializationContext ctxt){
         Pair<String, String> output = null;
