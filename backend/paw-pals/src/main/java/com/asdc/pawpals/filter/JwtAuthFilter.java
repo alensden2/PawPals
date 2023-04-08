6a066d68234fbc742e2e0722f899df5f1c4c4bc4
Alen John <alensden@gmail.com>
2023-04-08 16:59:54 -0300

    impl smell JwtAuthFilter --doInternalFilter

--- backend/paw-pals/src/main/java/com/asdc/pawpals/filter/JwtAuthFilter.java
+++ backend/paw-pals/src/main/java/com/asdc/pawpals/filter/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,37 +15,48 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
+  @Autowired
+  private JwtConfig jwtConfig;
 
-    @Autowired
-    private JwtConfig jwtConfig;
+  @Autowired
+  UserDetailsService userDetailService;
 
-    @Autowired
-    UserDetailsService userDetailService;
+  private final int SUBSTRING_INDEX = 7;
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authHeader = request.getHeader("Authorization");
-        String token = null;
-        String username = null;
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            token = authHeader.substring(7);
-            username = jwtConfig.extractUsername(token);
-        }
-
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = userDetailService.loadUserByUsername(username);
-            if (jwtConfig.validateToken(token, userDetails)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
-            }
-        }
-        filterChain.doFilter(request, response);
+  @Override
+  protected void doFilterInternal(
+    HttpServletRequest request,
+    HttpServletResponse response,
+    FilterChain filterChain
+  )
+    throws ServletException, IOException {
+    String authHeader = request.getHeader("Authorization");
+    String token = null;
+    String username = null;
+    if (authHeader != null && authHeader.startsWith("Bearer ")) {
+      token = authHeader.substring(SUBSTRING_INDEX);
+      username = jwtConfig.extractUsername(token);
     }
 
-
+    if (
+      username != null &&
+      SecurityContextHolder.getContext().getAuthentication() == null
+    ) {
+      UserDetails userDetails = userDetailService.loadUserByUsername(username);
+      if (jwtConfig.validateToken(token, userDetails)) {
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+          userDetails,
+          null,
+          userDetails.getAuthorities()
+        );
+        authToken.setDetails(
+          new WebAuthenticationDetailsSource().buildDetails(request)
+        );
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+      }
+    }
+    filterChain.doFilter(request, response);
+  }
 }
